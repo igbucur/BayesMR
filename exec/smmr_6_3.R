677072f3e1bd27392c8d85a2dcd7b0a9295a3cf8
gbucur <g.bucur@cs.ru.nl>
2019-08-22 17:05:54 +0200

    Cleaned up script for Subsection 6.3 and added code for calling Stan implementation

--- exec/smmr_6_3.R
+++ exec/smmr_6_3.R
@@ -12,10 +12,11 @@ library(ggthemes)
 
 # Deriving the sufficient statistics --------------------------------------
 
+genetic_associations <- read.csv('data/SNP_bmi_parkinson.csv')
 J <- nrow(genetic_associations)
 dat <- genetic_associations[order(genetic_associations$gamma_hat, decreasing = T)[1:J],]
 
-MAF <- dat$theta
+EAF <- dat$theta
 gamma_hat <- dat$gamma_hat
 gamma_se <- dat$gamma_se
 Gamma_hat <- dat$Gamma_hat
@@ -53,44 +54,51 @@ write_configuration_file(
   PolyChord_control = list(num_live_points = 500)
 )
 
+# WARNING: The posterior computation takes a lot of time (days) and requires a lot of memory.
 system('bin/polychord_MR ini/smmr_6_3.ini')
 
-beta <- quick_derive_beta(read.table('chains/smmr_6_2_fig_17_equal_weights.txt'), spike = 10000)
+# As a faster alternative, one can sample from the posterior using Stan,
+# since for this example we are not interested in computing the marginal likelihood.
+# library(rstan)
+# Pks_BMI_fit <- stan("src/BayesMR.stan", data = list(
+#   N = N,
+#   J = J,
+#   n = n,
+#   v_spike = 0.0001,
+#   v_slab = 1,
+#   SS = SSS,
+#   mu_X = 0,
+#   mu_Y = 0,
+#   eaf = EAF
+# ), iter = 50000, chains = 3, control = list(adapt_delta = 0.99, max_treedepth = 20))
+# beta <- as(Pks_BMI_fit, "matrix")[, 'beta', drop = FALSE]
+
+
+beta <- quick_derive_beta(read.table('chains/smmr_6_3_equal_weights.txt'), spike = 10000)
 beta <- as.matrix(beta)
 colnames(beta) <- 'beta'
 
-BW_FG_beta_plot <- mcmc_areas(beta, pars = "beta") +
-  theme_tufte() + coord_cartesian(xlim = c(-0.4, 0.2)) + theme(text = element_text(size = 30), axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
-  geom_vline(xintercept = -0.155) + geom_vline(xintercept = c(-0.223, -0.088), linetype = "dashed") + xlab(expression(beta)) + ylab("Posterior density") +
-  annotate(geom="text", x=-Inf, y=-Inf, label = "hat(beta)^{IVW}", hjust = -4.3, vjust = -3.1, parse = T, size = 10)
+Pks_BMI_beta_plot <- mcmc_areas(beta, pars = "beta") +
+  theme_tufte() + theme(text = element_text(size = 30), axis.title.x = element_text(margin = margin(t = 10)), axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
+  geom_vline(xintercept = -0.1946717) + geom_vline(xintercept = c(-0.3682688, -0.02107458), linetype = "dashed") + xlim(c(-0.5, 0.1)) +
+  annotate(geom="text", x=-Inf, y=-Inf, label = "hat(beta)^{IVW}", hjust = -4.6, vjust = -5, parse = T, size = 8) + ylab("Posterior density") +
+  xlab(expression(paste("Effect of BMI on the risk of PD (", beta, ")")))
 
-plot(BW_FG_beta_plot)
-ggsave('Manuscript-figure17.pdf', BW_FG_beta_plot, width = 10, height = 5)
+ggsave('Manuscript-figure19.pdf', Pks_BMI_beta_plot, width = 10, height = 5)
 
 
-# Second hyperparameter setting (lambda = 1e-4) ---------------------------
+# Reproduce outlier plot --------------------------------------------------
 
-write_configuration_file(
-  config_file_name = 'ini/smmr_6_2_fig_18.ini',
-  SS_filename = 'data/BW_FG_SSc.txt',
-  sigmaG_filename = 'data/BW_FG_SiG.txt',
-  num_instruments = 7,
-  num_observations = 855,
-  spike_precision = 10000,
-  model_type = 1,
-  PolyChord_control = list(num_live_points = 500)
-)
+outliers <- order(Gamma_hat)[1:2] # The outliers are the ones with the smallest association with the outcome
+colours <- rep('black', J); colours[outliers] <- 'red'
+shapes <- rep(1, J); shapes[outliers] <- 2
 
-system('bin/polychord_MR ini/smmr_6_2_fig_18.ini')
+Pks_BMI_outliers_plot <- ggplot(data.frame(Y = Gamma_hat, X = gamma_hat), aes(x = X, y = Y)) + 
+  geom_point(colour = colours, shape = shapes, size = 3) + 
+  geom_smooth(method = "lm", formula = y ~ 0 + x, se = FALSE, col = 'red', linetype = 2) +
+  geom_smooth(method = "lm", formula = y ~ 0 + x, se = FALSE, data = data.frame(Y = Gamma_hat, X = gamma_hat)[-outliers,], col = 'black') +
+  theme_tufte() + theme(text = element_text(size = 25), axis.title.x = element_text(margin = margin(t = 10)), axis.title.y = element_text(margin = margin(r = 10))) +
+  xlab("Genetic associations with the exposure") +
+  ylab("Genetic associations with the outcome")
 
-beta <- quick_derive_beta(read.table('chains/smmr_6_2_fig_18_equal_weights.txt'), spike = 1)
-beta <- as.matrix(beta)
-colnames(beta) <- 'beta'
-
-BW_FG_beta_plot <- mcmc_areas(beta, pars = "beta") +
-  theme_tufte() + coord_cartesian(xlim = c(-0.4, 0.2)) + theme(text = element_text(size = 30), axis.text.y = element_blank(), axis.ticks.y = element_blank()) +
-  geom_vline(xintercept = -0.155) + geom_vline(xintercept = c(-0.223, -0.088), linetype = "dashed") + xlab(expression(beta)) + ylab("Posterior density") +
-  annotate(geom="text", x=-Inf, y=-Inf, label = "hat(beta)^{IVW}", hjust = -4.3, vjust = -3.1, parse = T, size = 10)
-
-plot(BW_FG_beta_plot)
-ggsave('Manuscript-figure18.pdf', BW_FG_beta_plot, width = 10, height = 5)
+ggsave('Manuscript-figure20.pdf', Pks_BMI_outliers_plot, fonts = "serif", width = 10, height = 7)
